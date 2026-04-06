20e52e405d6017f6d8048692b32da4acbdcd7bbc
HirakiriShogun <hirakirishogun@gmail.com>
2026-04-06 12:22:42 +0300

    feat: add BFF endpoints, caching and external integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { BffModule } from './modules/bff/bff.module';
 import { CommentsModule } from './modules/comments/comments.module';
 import { TaskTrackerGraphqlModule } from './modules/graphql/graphql.module';
+import { IntegrationsModule } from './modules/integrations/integrations.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { ProjectsModule } from './modules/projects/projects.module';
 import { TasksModule } from './modules/tasks/tasks.module';
@@ -11,6 +13,8 @@ import { WorkspacesModule } from './modules/workspaces/workspaces.module';
 @Module({
   imports: [
     PrismaModule,
+    BffModule,
+    IntegrationsModule,
     UsersModule,
     WorkspacesModule,
     WorkspaceMembersModule,
---
+++ src/modules/bff/bff.constants.ts
@@ -0,0 +1,1 @@
+export const BFF_CACHE_TTL = 30_000;
